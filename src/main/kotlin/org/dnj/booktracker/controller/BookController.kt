83fe2a04e382d04a95d4999047aeb0cbde64d6d7
Daniil Bubnov <bubnov.d.e@gmail.com>
2019-08-17 23:20:15 +0300

    add cross origin

--- src/main/kotlin/org/dnj/booktracker/controller/BookController.kt
+++ src/main/kotlin/org/dnj/booktracker/controller/BookController.kt
@@ -3,12 +3,10 @@ package org.dnj.booktracker.controller
 import org.dnj.booktracker.BookRecord
 import org.dnj.booktracker.repo.BookRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin
 class BookController(
     @Autowired val bookRepo: BookRepository
 ) {
