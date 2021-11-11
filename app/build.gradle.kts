8a0d23d64d733f8034230849b7737d4a32c8afd2
demoth <bubnov.d.e@gmail.com>
2021-11-11 20:32:15 +0100

    move to the spring actuator health endpoint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,9 +18,11 @@ repositories {
 
 dependencies {
     implementation("ch.qos.logback:logback-classic:1.2.6")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
